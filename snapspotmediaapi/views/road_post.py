36a4a2c86e1104cc3e4e586e12ad030736002d9e
madison-hepner <hepnermadison@icloud.com>
2022-09-02 16:23:52 -0500

    road posts create having same issue as locationpost

--- snapspotmediaapi/views/road_post.py
+++ snapspotmediaapi/views/road_post.py
@@ -5,7 +5,9 @@ from rest_framework.response import Response
 from rest_framework import serializers, status
 from django.core.exceptions import ValidationError
 from snapspotmediaapi.models import RoadPost
-from snapspotmediaapi.models.driver import Driver
+from snapspotmediaapi.models import Driver
+from snapspotmediaapi.models import RoadType
+from snapspotmediaapi.models.location import Location
 
 
 class RoadPostView(ViewSet):
@@ -18,6 +20,28 @@ class RoadPostView(ViewSet):
     def list(self, request):
         road_posts = RoadPost.objects.all()
         serializer = RoadPostSerializer(road_posts, many=True)
+        return Response(serializer.data)
+    
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized game instance
+        """
+        driver = Driver.objects.get(user=request.auth.user)
+        road_type = RoadType.objects.get(pk=request.data["road_type"])
+        locationId = Location.objects.get(pk=request.data["locationId"])
+
+        road_post = RoadPost.objects.create(
+            road_name=request.data["road_name"],
+            description=request.data["description"],
+            locationImg=request.data["locationImg"],
+            skill_level=request.data["skill_level"],
+            driver=driver,
+            locationId=locationId,
+            road_type=road_type
+        )
+        serializer = RoadPostSerializer(road_post)
         return Response(serializer.data)
 
 
